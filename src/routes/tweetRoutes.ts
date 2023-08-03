c661540e9c0b9c92dc9ba219703f9e7457d69a83
momin37 <muzammilsaeed49@gmail.com>
2023-08-04 01:24:07 +0500

    tweets crud is done

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,53 +1,87 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient()
 
 
 // create tweet
 
-router.post("/",(req,res)=>{
-    res.status(501).json({
-        error: "mother fucker"
+router.post("/", async (req, res) => {
+
+    const { content, image, userId } = req.body;
+
+    const result = await prisma.tweet.create({
+        data: {
+            content, image, userId
+        }
     })
+
+    res.json(result)
+
+
 })
 
 
 // list of tweets
 
-router.get("/",(req, res)=>{
-    res.status(501).json({
-        error:"mother fucker"
-    })
+router.get("/", async (req, res) => {
+
+    const allTweets = await prisma.tweet.findMany({ include: { user: true } })
+
+    if (!allTweets) {
+        return res.status(404).json({ error: "Tweets not found" })
+    }
+
+    res.json(allTweets)
 })
 
 // get tweet
 
-router.get("/:id",(req,res)=>{
-    const {id} = req.params
-    res.status(501).json({
-        error:"not found"
+router.get("/:id", async (req, res) => {
+
+    const { id } = req.params
+    const tweet = await prisma.tweet.findUnique({
+        where: {
+            id: Number(id)
+        }
     })
+    if (!tweet) {
+        return res.status(404).json({ error: "Tweet not found" })
+    }
+
+    res.json(tweet)
 })
 
 // update tweet
 
-router.put("/:id",(req,res)=>{
-    const {id} = req.params;
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+
+    // const {}
 
     res.status(501).json({
-        error:"not found"
+        error: "not found"
     })
 })
 
 
 // delete tweet
 
-router.delete("/:id",(req,res)=>{
-    const {id} = req.params;
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
 
-    res.status(501).json({
-        error:"not found"
-    })
+    try {
+        await prisma.tweet.delete({ where: { id: Number(id) } })
+        res.sendStatus(200)
+    }
+    catch (e) {
+        res.status(400).json({
+            error: "Failed to delete the tweet"
+        })
+    }
+
+
 })
 
 
